b320f97439ab075fbc1e4b14e31509eb400c5980
Solveig Andres <s9soandr@users.noreply.github.com>
2014-11-15 17:09:27 +0100

    Alle bisherigen Dateien hinzugefügt

---
+++ Graph.h
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <list>
+
+
+#ifndef GRAPH_H_INCLUDED
+#define GRAPH_H_INCLUDED
+
+class Node;
+class Edge;
+class Sequence;
+
+
+class Graph{
+private:
+    std::list<Node> nodes;
+    //This methods adds a node to the graph it simultaneously already builds the edges
+    Node& insertNode(Node &n);
+    
+
+public:
+
+	Graph();
+	
+	Graph(const std::list<Node> n);
+
+    //Returns true if a node with the given sequence exists
+    bool hasNode(const Sequence& seq) const;
+
+    //Returns the node with the given sequence
+    //It builds a node if it is not existing
+    Node& getNode(const Sequence& seq);
+
+    //Removes the given node
+    void removeNode(Node& node);
+
+    //Returns the edge from source to target
+    Edge getEdge(Node& src, Node& target);
+
+    //Not yet decided
+    Edge getEdge(const Sequence& src, const Sequence& target);
+
+    //Returns the graph as graphviz format
+    friend std::ostream& operator << (std::ostream& strm, const Graph& graph);
+
+    //Converts the graph from the graphviz format to our graph format
+    friend std::istream& operator >> (std::istream& strm, Graph& graph);
+    
+    std::list<Node> getNodes();
+    
+    // returns alle Edges
+    std::list<Edge> getEdges();
+
+
+};
+
+#endif // GRAPH_H_INCLUDED
